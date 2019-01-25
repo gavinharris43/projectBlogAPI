2c68bd6f773665d286014b35b77d5a3b4e3a8af4
gavinharris43 <gavinharris43@googlemail.com>
2019-01-25 09:12:45 +0000

    account added

--- ProjectBlog/deploy.sh
+++ ProjectBlog/deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mvn clean install
-cp target/movies1.war cd Users\Admin\Maven_Home\wildfly-10.1.0.Final\standalone\deployments
+cp target/movies1.war  ~/Maven_Home/wildfly-10.1.0.Final/standalone/deployments
+sudo systemctl restart wildfly
---
+++ ProjectBlog/src/main/java/com/qa/persistence/domain/Account.java
@@ -0,0 +1,84 @@
+package com.qa.persistence.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity 
+public class Account {
+	
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Id
+	private Long id;
+	private String username;
+	private String firstName;
+	private String lastName;
+	private String password;
+	private String userLevel;
+	
+	public Account() {
+	
+	}
+	
+	public Account(Long id, String username, String firstName, String lastName, String password, String userLevel) {
+		this.id = id;
+		this.username = username;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.password = password;
+		this.userLevel= userLevel;
+	}
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getUserLevel() {
+		return userLevel;
+	}
+
+	public void setUserLevel(String userLevel) {
+		this.userLevel = userLevel;
+	}
+
+
+
+}
+
+
+
