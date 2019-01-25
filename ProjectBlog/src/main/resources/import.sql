9fb100be68d62ae189ddefe7a30426086af7a417
gavinharris43 <gavinharris43@googlemail.com>
2019-01-25 15:49:09 +0000

    sql

---
+++ ProjectBlog/src/main/resources/import.sql
@@ -0,0 +1,3 @@
+INSERT INTO ACCOUNT (id, username, firstName, lastName, password, userLevel) VALUES (1, 'gavinharris43', 'Gavin','Harris', 'password','0');
+INSERT INTO BLOG (id, title, author, content,date ,category) VALUES (1, 'TSL Shutdown!', 'gavinharris43','To accompany her sixth album', '2019/01/01','TS');
+INSERT INTO BLOG (id, title, author, content,date ,category) VALUES (2, 'TSL Shutdown!', 'gavinharris43','To accompany her sixth album', '2019/01/01','Guitar');
