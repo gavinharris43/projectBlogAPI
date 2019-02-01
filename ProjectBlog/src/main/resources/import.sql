14f48c0ed03204ddf8c1b4bbf80c7a0c9e87a7fc
gavinharris43 <gavinharris43@googlemail.com>
2019-02-01 15:50:43 +0000

    finnished Yey

--- ProjectBlog/src/main/resources/import.sql
+++ ProjectBlog/src/main/resources/import.sql
@@ -1,3 +1,6 @@
-INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (1, 'TSL Shutdown!', 'gavinharris43','To accompany her sixth album', '2019/01/01','TS');
-INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (2, 'TSL Shutdown!', 'gavinharris43','To accompany her sixth album', '2019/01/01','Guitar');
+INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (1, 'TSL Shutdown!', 'admin','Hello World TS', '2019/01/01','TS');
+INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (2, 'TSL Shutdown!', 'admin','Hello World Guitar ', '2019/01/01','Guitar');
+INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (3, 'TSL Shutdown!', 'gavinharris43','To accompany her sixth album, Taylor ', '2019/01/01','TS');
+INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (4, 'New Guitar', 'gavinharris43','New Les Paul', '2019/01/01','Guitar');
 INSERT INTO ACCOUNT (id, username, firstName, lastName, password, userLevel) VALUES (1, 'gavinharris43', 'Gavin','Harris', 'password','0');
+INSERT INTO ACCOUNT (id, username, firstName, lastName, password, userLevel) VALUES (2, 'admin', 'Gavin','Harris', 'password','1');
