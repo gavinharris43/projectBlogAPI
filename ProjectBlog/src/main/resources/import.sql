42b2af1e015b706f0252b72c3688639b68fe19e1
gavinharris43 <gavinharris43@googlemail.com>
2019-02-01 15:52:02 +0000

    finished api

--- ProjectBlog/src/main/resources/import.sql
+++ ProjectBlog/src/main/resources/import.sql
@@ -1,5 +1,5 @@
-INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (1, 'TSL Shutdown!', 'admin','Hello World TS', '2019/01/01','TS');
-INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (2, 'TSL Shutdown!', 'admin','Hello World Guitar ', '2019/01/01','Guitar');
+INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (1, 'Hello World', 'admin','Hello World TS', '2019/01/01','TS');
+INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (2, 'Hello World', 'admin','Hello World Guitar ', '2019/01/01','Guitar');
 INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (3, 'TSL Shutdown!', 'gavinharris43','To accompany her sixth album, Taylor ', '2019/01/01','TS');
 INSERT INTO BLOG (id, title, username, content,date ,category) VALUES (4, 'New Guitar', 'gavinharris43','New Les Paul', '2019/01/01','Guitar');
 INSERT INTO ACCOUNT (id, username, firstName, lastName, password, userLevel) VALUES (1, 'gavinharris43', 'Gavin','Harris', 'password','0');
